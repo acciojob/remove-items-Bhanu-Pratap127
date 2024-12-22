0a947f0ee599004b699430d90eabe6d8fef8ed2a
Bhanu-Pratap127 <bhanupartap1273@gmail.com>
2024-12-22 15:59:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn")
+	btn.addEventListener("click", removeColor);
+
+function removeColor() {
+	const colorSelect = document.getElementById("colorSelect");
+	colorSelect.options[colorSelect.selectedIndex].remove();
+}
